904d6ae9dbb25b51061717cbf2a8446f9263ae50
Pieter Wuille <pieter.wuille@gmail.com>
2017-02-15 17:45:22 -0800

    Switch FastRandomContext to ChaCha20

--- src/test/test_wagerr.cpp
+++ src/test/test_wagerr.cpp
@@ -25,7 +25,8 @@
 CClientUIInterface uiInterface;
 CWallet* pwalletMain;
 
-FastRandomContext insecure_rand_ctx(true);
+uint256 insecure_rand_seed = GetRandHash();
+FastRandomContext insecure_rand_ctx(insecure_rand_seed);
 
 extern bool fPrintToConsole;
 extern void noui_connect();
