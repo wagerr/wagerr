e40168e02f23d0cf88e5018fa86aea667d028d9d
Fuzzbawls <fuzzbawls@gmail.com>
2018-02-07 22:55:36 -0800

    build: Deduplicate version numbers
    Reduce the number of instances where the version number needs to be
    updated:
    
    - Doxygen client version is now generated by the build system
    - clientversion.h now either pulls from pivx-config.h or not at all
    
    Also update to newer Doxygen version

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Copyright (c) 2018 The Wagerr developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -8,28 +8,12 @@
 
 #if defined(HAVE_CONFIG_H)
 #include "config/wagerr-config.h"
-#else
-
-/**
- * client versioning and copyright year
- */
-
-//! These need to be macros, as clientversion.cpp's and wagerr*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 4
-#define CLIENT_VERSION_REVISION 34
-#define CLIENT_VERSION_BUILD 3
-
-//! Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE true
-
-/**
- * Copyright year (2009-this)
- * Todo: update this when changing our copyright comments in the source
- */
-#define COPYRIGHT_YEAR 2018
-
 #endif //HAVE_CONFIG_H
+
+// Check that required client information is defined
+#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_REVISION) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
+#error Client version information missing: version is not defined by wagerr-config.h or in any other way
+#endif
 
 /**
  * Converts the parameter X to a string after macro replacement on X has been performed.
