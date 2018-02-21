c6030474f66c41a56bf0a5a6b4d29b097191030a
Rocco Stanzione <grasshopper@linuxkungfu.org>
2018-02-21 16:04:29 -0600

    Update 2 more examples

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -32,7 +32,7 @@ bool CMasternodeConfig::read(std::string& strErr)
             std::string strHeader = "#Multi masternode config\n"
                                     "#=======================\n"
                                     "# \n"
-                                    "#The multi masternode config allows you to control multiple masternodes from a single wallet. The wallet needs to have a valid collateral output of 10000 coins for each masternode. To use this, place a file named masternode.conf in the data directory of your install:\n"
+                                    "#The multi masternode config allows you to control multiple masternodes from a single wallet. The wallet needs to have a valid collateral output of 25000 coins for each masternode. To use this, place a file named masternode.conf in the data directory of your install:\n"
                                     "# * Windows: %APPDATA%\wagerr\\n"
                                     "# * Mac OS: ~/Library/Application Support/wagerr/\n"
                                     "# * Unix/Linux: ~/.wagerr/\n"
