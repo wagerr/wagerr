87fc9afeda00d1856a71e46f27f19aa3395e06ad
Cryptarchist <cryptarchist@gmail.com>
2018-02-23 16:27:28 +1000

    Update icons

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # create multiresolution windows icon
 #mainnet
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/wagerr.png
+ICON_DST=../../src/qt/res/icons/wagerr.ico
+convert ${ICON_SRC} -resize 16x16 wagerr-16.png
+convert ${ICON_SRC} -resize 32x32 wagerr-32.png
+convert ${ICON_SRC} -resize 48x48 wagerr-48.png
+convert ${ICON_SRC} -resize 256x256 wagerr-256.png
+convert wagerr-16.png wagerr-32.png wagerr-48.png wagerr-256.png ${ICON_DST}
 #testnet
-ICON_SRC=../../src/qt/res/icons/bitcoin_testnet.png
-ICON_DST=../../src/qt/res/icons/bitcoin_testnet.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
-rm bitcoin-16.png bitcoin-32.png bitcoin-48.png
+ICON_SRC=../../src/qt/res/icons/wagerr_testnet.png
+ICON_DST=../../src/qt/res/icons/wagerr_testnet.ico
+convert ${ICON_SRC} -resize 16x16 wagerr-16.png
+convert ${ICON_SRC} -resize 32x32 wagerr-32.png
+convert ${ICON_SRC} -resize 48x48 wagerr-48.png
+convert ${ICON_SRC} -resize 256x256 wagerr-256.png
+convert wagerr-16.png wagerr-32.png wagerr-48.png wagerr-256.png ${ICON_DST}
+rm wagerr-16.png wagerr-32.png wagerr-48.png wagerr-256.png
