d8f6d71b7e2e62b8e66de4023bd1849e31d55694
presstab <presstab1337@gmail.com>
2018-03-17 12:27:15 -0600

    Add description for -wagerrstake= and -zwgrstake=.

--- src/init.cpp
+++ src/init.cpp
@@ -507,6 +507,8 @@ std::string HelpMessage(HelpMessageMode mode)
 #ifdef ENABLE_WALLET
     strUsage += HelpMessageGroup(_("Staking options:"));
     strUsage += HelpMessageOpt("-staking=<n>", strprintf(_("Enable staking functionality (0-1, default: %u)"), 1));
+    strUsage += HelpMessageOpt("-wagerrstake=<n>", strprintf(_("Enable or disable staking functionality for WGR inputs (0-1, default: %u)"), 1));
+    strUsage += HelpMessageOpt("-zwgrstake=<n>", strprintf(_("Enable or disable staking functionality for zWGR inputs (0-1, default: %u)"), 1));
     strUsage += HelpMessageOpt("-reservebalance=<amt>", _("Keep the specified amount available for spending at all times (default: 0)"));
     if (GetBoolArg("-help-debug", false)) {
         strUsage += HelpMessageOpt("-printstakemodifier", _("Display the stake modifier calculations in the debug.log file."));
