222dd5dd33d2be0f1d14633e3740382d19472274
Fuzzbawls <fuzzbawls@gmail.com>
2018-03-22 22:11:22 -0700

    Fix typo in ban reason output

--- src/net.h
+++ src/net.h
@@ -265,7 +265,7 @@ public:
     {
         switch (banReason) {
         case BanReasonNodeMisbehaving:
-            return "node misbehabing";
+            return "node misbehaving";
         case BanReasonManuallyAdded:
             return "manually added";
         default:
