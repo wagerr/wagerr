d93056c8d6cae7c817190e369e96bb34d8b61bbd
Fuzzbawls <fuzzbawls@gmail.com>
2018-04-05 23:42:33 -0700

    Fix another instance of a wayward eye

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -95,7 +95,9 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet* 
                 if (!fZSpendFromMe)
                     continue;
 
+                isminetype mine = wallet->IsMine(txout);
                 TransactionRecord sub(hash, nTime);
+                sub.involvesWatchAddress = mine & ISMINE_WATCH_ONLY;
                 sub.type = TransactionRecord::ZerocoinSpend_Change_zWgr;
                 sub.address = mapValue["zerocoinmint"];
                 if (!fFeeAssigned) {
