eons-finance/ALQO

implemented masternode_broadcast

src/rpcserver.cpp
@@ -356,6 +356,7 @@ static const CRPCCommand vRPCCommands[] =
         {"pivx", "listmasternodes", &listmasternodes, true, true, false},
         {"pivx", "getmasternodecount", &getmasternodecount, true, true, false},
         {"pivx", "masternodeconnect", &masternodeconnect, true, true, false},
+		{"pivx", "masternodebroadcast", &masternodebroadcast, true, true, false},
         {"pivx", "masternodecurrent", &masternodecurrent, true, true, false},
         {"pivx", "masternodedebug", &masternodedebug, true, true, false},
         {"pivx", "startmasternode", &startmasternode, true, true, false},

wagerr/wagerr

src/rpcserver.cpp
@@ -357,6 +357,7 @@ static const CRPCCommand vRPCCommands[] =
         {"wagerr", "listmasternodes", &listmasternodes, true, true, false},
         {"wagerr", "getmasternodecount", &getmasternodecount, true, true, false},
         {"wagerr", "masternodeconnect", &masternodeconnect, true, true, false},
+		{"wagerr", "masternodebroadcast", &masternodebroadcast, true, true, false},
         {"wagerr", "masternodecurrent", &masternodecurrent, true, true, false},
         {"wagerr", "masternodedebug", &masternodedebug, true, true, false},
         {"wagerr", "startmasternode", &startmasternode, true, true, false},