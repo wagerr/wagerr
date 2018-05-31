eons-finance/ALQO

Database mints generated by the mintpool.
- Load all generated mints on initialization. Then remove mints from the mintpool that are already known.

src/init.cpp
@@ -1647,6 +1647,8 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
         //Load zerocoin mint hashes to memory
         CWalletDB(pwalletMain->strWalletFile).ListMintedCoins(true, true, true, pwalletMain->zpivTracker);
+        zwalletMain->LoadMintPoolFromDB();
+        zwalletMain->RemoveMintsFromPool(pwalletMain->zpivTracker->GetSerialHashes());
         zwalletMain->SyncWithChain();
     }  // (!fDisableWallet)
 #else  // ENABLE_WALLET

src/walletdb.cpp
@@ -1146,6 +1146,68 @@ bool CWalletDB::ReadZPIVCount(uint32_t& nCount)
     return Read(string("dzc"), nCount);
 }
 
+bool CWalletDB::WriteMintPoolPair(const uint256& hashMasterSeed, const uint256& hashPubcoin, const uint32_t& nCount)
+{
+    return Write(make_pair(string("mintpool"), hashPubcoin), make_pair(hashMasterSeed, nCount));
+}
+
+//! map with hashMasterSeed as the key, paired with vector of hashPubcoins and their count
+std::map<uint256, std::vector<pair<uint256, uint32_t> > > CWalletDB::MapMintPool()
+{
+    std::map<uint256, std::vector<pair<uint256, uint32_t> > > mapPool;
+    Dbc* pcursor = GetCursor();
+    if (!pcursor)
+        throw runtime_error(std::string(__func__)+" : cannot create DB cursor");
+    unsigned int fFlags = DB_SET_RANGE;
+    for (;;)
+    {
+        // Read next record
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        if (fFlags == DB_SET_RANGE)
+            ssKey << make_pair(string("mintpool"), uint256(0));
+        CDataStream ssValue(SER_DISK, CLIENT_VERSION);
+        int ret = ReadAtCursor(pcursor, ssKey, ssValue, fFlags);
+        fFlags = DB_NEXT;
+        if (ret == DB_NOTFOUND)
+            break;
+        else if (ret != 0)
+        {
+            pcursor->close();
+            throw runtime_error(std::string(__func__)+" : error scanning DB");
+        }
+
+        // Unserialize
+        string strType;
+        ssKey >> strType;
+        if (strType != "mintpool")
+            break;
+
+        uint256 hashPubcoin;
+        ssKey >> hashPubcoin;
+
+        uint256 hashMasterSeed;
+        ssValue >> hashMasterSeed;
+
+        uint32_t nCount;
+        ssValue >> nCount;
+
+        pair<uint256, uint32_t> pMint;
+        pMint.first = hashPubcoin;
+        pMint.second = nCount;
+        if (mapPool.count(hashMasterSeed)) {
+            mapPool.at(hashMasterSeed).emplace_back(pMint);
+        } else {
+            vector<pair<uint256, uint32_t> > vPairs;
+            vPairs.emplace_back(pMint);
+            mapPool.insert(make_pair(hashMasterSeed, vPairs));
+        }
+    }
+
+    pcursor->close();
+
+    return mapPool;
+}
+
 std::list<CZerocoinMint> CWalletDB::ListMintedCoins(bool fUnusedOnly, bool fMaturedOnly, bool fUpdateStatus, CzPIVTracker* zpivTracker)
 {
     std::list<CZerocoinMint> listPubCoin;
@@ -1262,12 +1324,17 @@ std::list<CZerocoinMint> CWalletDB::ListMintedCoins(bool fUnusedOnly, bool fMatu
     pcursor->close();
 
     //overwrite any updates
-    for (CZerocoinMint& mint : vOverWrite)
-        zpivTracker->UpdateMint(mint);
+    for (CZerocoinMint& mint : vOverWrite) {
+        zpivTracker->UpdateMint(mint, false);
+        WriteZerocoinMint(mint);
+    }
 
     // archive mints
-    for (CZerocoinMint& mint : vArchive)
-        zpivTracker->Archive(mint);
+    for (CZerocoinMint& mint : vArchive) {
+        zpivTracker->Archive(mint, false);
+        ArchiveMintOrphan(mint);
+    }
+
 
     return listPubCoin;
 }

wagerr/wagerr

src/init.cpp
@@ -1642,6 +1642,8 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
         //Load zerocoin mint hashes to memory
         CWalletDB(pwalletMain->strWalletFile).ListMintedCoins(true, true, true, pwalletMain->zwgrTracker);
+        zwalletMain->LoadMintPoolFromDB();
+        zwalletMain->RemoveMintsFromPool(pwalletMain->zwgrTracker->GetSerialHashes());
         zwalletMain->SyncWithChain();
     }  // (!fDisableWallet)
 #else  // ENABLE_WALLET

src/walletdb.cpp
@@ -1147,6 +1147,68 @@ bool CWalletDB::ReadZWGRCount(uint32_t& nCount)
     return Read(string("dzc"), nCount);
 }
 
+bool CWalletDB::WriteMintPoolPair(const uint256& hashMasterSeed, const uint256& hashPubcoin, const uint32_t& nCount)
+{
+    return Write(make_pair(string("mintpool"), hashPubcoin), make_pair(hashMasterSeed, nCount));
+}
+
+//! map with hashMasterSeed as the key, paired with vector of hashPubcoins and their count
+std::map<uint256, std::vector<pair<uint256, uint32_t> > > CWalletDB::MapMintPool()
+{
+    std::map<uint256, std::vector<pair<uint256, uint32_t> > > mapPool;
+    Dbc* pcursor = GetCursor();
+    if (!pcursor)
+        throw runtime_error(std::string(__func__)+" : cannot create DB cursor");
+    unsigned int fFlags = DB_SET_RANGE;
+    for (;;)
+    {
+        // Read next record
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        if (fFlags == DB_SET_RANGE)
+            ssKey << make_pair(string("mintpool"), uint256(0));
+        CDataStream ssValue(SER_DISK, CLIENT_VERSION);
+        int ret = ReadAtCursor(pcursor, ssKey, ssValue, fFlags);
+        fFlags = DB_NEXT;
+        if (ret == DB_NOTFOUND)
+            break;
+        else if (ret != 0)
+        {
+            pcursor->close();
+            throw runtime_error(std::string(__func__)+" : error scanning DB");
+        }
+
+        // Unserialize
+        string strType;
+        ssKey >> strType;
+        if (strType != "mintpool")
+            break;
+
+        uint256 hashPubcoin;
+        ssKey >> hashPubcoin;
+
+        uint256 hashMasterSeed;
+        ssValue >> hashMasterSeed;
+
+        uint32_t nCount;
+        ssValue >> nCount;
+
+        pair<uint256, uint32_t> pMint;
+        pMint.first = hashPubcoin;
+        pMint.second = nCount;
+        if (mapPool.count(hashMasterSeed)) {
+            mapPool.at(hashMasterSeed).emplace_back(pMint);
+        } else {
+            vector<pair<uint256, uint32_t> > vPairs;
+            vPairs.emplace_back(pMint);
+            mapPool.insert(make_pair(hashMasterSeed, vPairs));
+        }
+    }
+
+    pcursor->close();
+
+    return mapPool;
+}
+
 std::list<CZerocoinMint> CWalletDB::ListMintedCoins(bool fUnusedOnly, bool fMaturedOnly, bool fUpdateStatus, CzWGRTracker* zwgrTracker)
 {
     std::list<CZerocoinMint> listPubCoin;
@@ -1263,12 +1325,17 @@ std::list<CZerocoinMint> CWalletDB::ListMintedCoins(bool fUnusedOnly, bool fMatu
     pcursor->close();
 
     //overwrite any updates
-    for (CZerocoinMint& mint : vOverWrite)
-        zwgrTracker->UpdateMint(mint);
+    for (CZerocoinMint& mint : vOverWrite) {
+        zwgrTracker->UpdateMint(mint, false);
+        WriteZerocoinMint(mint);
+    }
 
     // archive mints
-    for (CZerocoinMint& mint : vArchive)
-        zwgrTracker->Archive(mint);
+    for (CZerocoinMint& mint : vArchive) {
+        zwgrTracker->Archive(mint, false);
+        ArchiveMintOrphan(mint);
+    }
+
 
     return listPubCoin;
 }