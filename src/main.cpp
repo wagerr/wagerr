eons-finance/ALQO

Do not rely on chainActive with contextual checks.
This can lead to situations of rejecting valid blocks that are from a competing fork.

src/main.cpp
@@ -4657,19 +4657,21 @@ bool AcceptBlockHeader(const CBlock& block, CValidationState& state, CBlockIndex
     return true;
 }
 
-bool ContextualCheckZerocoinStake(CStakeInput* stake)
+bool ContextualCheckZerocoinStake(int nHeight, CStakeInput* stake)
 {
-    if (chainActive.Height() + 1 < Params().Zerocoin_Block_V2_Start())
+    if (nHeight < Params().Zerocoin_Block_V2_Start())
         return error("%s: zPIV stake block is less than allowed start height", __func__);
 
     if (CZPivStake* zPIV = dynamic_cast<CZPivStake*>(stake)) {
-        CBlockIndex* pindex = zPIV->GetIndexFrom();
+        CBlockIndex* pindexFrom = zPIV->GetIndexFrom();
+        if (!pindexFrom)
+            return error("%s: failed to get index associated with zPIV stake checksum", __func__);
 
-        if (chainActive.Height() - pindex->nHeight < Params().Zerocoin_RequiredStakeDepth())
+        if (chainActive.Height() - pindexFrom->nHeight < Params().Zerocoin_RequiredStakeDepth())
             return error("%s: zPIV stake does not have required confirmation depth", __func__);
 
         //The checksum needs to be the exact checksum from 200 blocks ago
-        uint256 nCheckpoint200 = chainActive[chainActive.Height() - Params().Zerocoin_RequiredStakeDepth()]->nAccumulatorCheckpoint;
+        uint256 nCheckpoint200 = chainActive[nHeight - Params().Zerocoin_RequiredStakeDepth()]->nAccumulatorCheckpoint;
         uint32_t nChecksum200 = ParseChecksum(nCheckpoint200, libzerocoin::AmountToZerocoinDenomination(zPIV->GetValue()));
         if (nChecksum200 != zPIV->GetChecksum())
             return error("%s: accumulator checksum is different than the block 200 blocks previous. stake=%d block200=%d", __func__, zPIV->GetChecksum(), nChecksum200);
@@ -4722,7 +4724,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         if (!stake)
             return error("%s: null stake ptr", __func__);
 
-        if (stake->IsZPIV() && !ContextualCheckZerocoinStake(stake.get()))
+        if (stake->IsZPIV() && !ContextualCheckZerocoinStake(pindexPrev->nHeight, stake.get()))
             return state.DoS(100, error("%s: staked zPIV fails context checks", __func__));
 
         uint256 hash = block.GetHash();

wagerr/wagerr

src/main.cpp
@@ -4461,19 +4461,21 @@ bool AcceptBlockHeader(const CBlock& block, CValidationState& state, CBlockIndex
     return true;
 }
 
-bool ContextualCheckZerocoinStake(CStakeInput* stake)
+bool ContextualCheckZerocoinStake(int nHeight, CStakeInput* stake)
 {
-    if (chainActive.Height() + 1 < Params().Zerocoin_Block_V2_Start())
+    if (nHeight < Params().Zerocoin_Block_V2_Start())
         return error("%s: zWGR stake block is less than allowed start height", __func__);
 
     if (CZWgrStake* zWGR = dynamic_cast<CZWgrStake*>(stake)) {
-        CBlockIndex* pindex = zWGR->GetIndexFrom();
+        CBlockIndex* pindexFrom = zWGR->GetIndexFrom();
+        if (!pindexFrom)
+            return error("%s: failed to get index associated with zWGR stake checksum", __func__);
 
-        if (chainActive.Height() - pindex->nHeight < Params().Zerocoin_RequiredStakeDepth())
+        if (chainActive.Height() - pindexFrom->nHeight < Params().Zerocoin_RequiredStakeDepth())
             return error("%s: zWGR stake does not have required confirmation depth", __func__);
 
         //The checksum needs to be the exact checksum from 200 blocks ago
-        uint256 nCheckpoint200 = chainActive[chainActive.Height() - Params().Zerocoin_RequiredStakeDepth()]->nAccumulatorCheckpoint;
+        uint256 nCheckpoint200 = chainActive[nHeight - Params().Zerocoin_RequiredStakeDepth()]->nAccumulatorCheckpoint;
         uint32_t nChecksum200 = ParseChecksum(nCheckpoint200, libzerocoin::AmountToZerocoinDenomination(zWGR->GetValue()));
         if (nChecksum200 != zWGR->GetChecksum())
             return error("%s: accumulator checksum is different than the block 200 blocks previous. stake=%d block200=%d", __func__, zWGR->GetChecksum(), nChecksum200);
@@ -4526,7 +4528,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         if (!stake)
             return error("%s: null stake ptr", __func__);
 
-        if (stake->IsZWGR() && !ContextualCheckZerocoinStake(stake.get()))
+        if (stake->IsZWGR() && !ContextualCheckZerocoinStake(pindexPrev->nHeight, stake.get()))
             return state.DoS(100, error("%s: staked zWGR fails context checks", __func__));
 
         uint256 hash = block.GetHash();