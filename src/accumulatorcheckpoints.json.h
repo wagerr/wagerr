33750425ddb5ff3a837a8e260c2293d23d0d43cf
wagerrtor <tor@i2pmail.org>
2018-06-27 04:02:33 +0200

    Initialize accumulator checkpoints

--- src/accumulatorcheckpoints.json.h
+++ src/accumulatorcheckpoints.json.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2018 The PIVX developers
+// Copyright (c) 2018 The Wagerr Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
