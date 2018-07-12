11f4f391f1345415618d58fd2f15dfcb006508fe
WagerrTor <35894915+WagerrTor@users.noreply.github.com>
2018-07-12 11:30:25 +0200

    Update main.h
    fix merging issue

--- src/main.h
+++ src/main.h
@@ -238,12 +238,7 @@ bool DisconnectBlocksAndReprocess(int blocks);
 
 // ***TODO***
 double ConvertBitsToDouble(unsigned int nBits);
-<<<<<<< HEAD
 int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCount, bool isZWGRStake);
-=======
-int64_t GetBlockPayouts( std::vector<CTxOut>& vexpectedPayouts, CAmount& nMNBetReward);
-int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCount = 0);
->>>>>>> 72d065ded5d287371c32c6f5b0d5e5186d84ac33
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader* pblock, bool fProofOfStake);
 
 bool ActivateBestChain(CValidationState& state, CBlock* pblock = NULL, bool fAlreadyChecked = false);
