e7332b1b8e07fa55b4d21844df0a85f8d8c271de
Kokary <kokary@servitising.org>
2018-07-25 20:09:19 +0200

    [FIX] deduct masternode payment from vout[1] instead of last vout

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -324,11 +324,11 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFe
             unsigned int i = txNew.vout.size();
             txNew.vout.resize(i + 1);
             txNew.vout[i].scriptPubKey = payee;
-            txNew.vout[i].nValue = masternodePayment;
+            txNew.vout[i].nValue = masternodePayment + nFees;
 
             //subtract mn payment from the stake reward
             if (!txNew.vout[1].IsZerocoinMint())
-                txNew.vout[i - 1].nValue -= masternodePayment;
+                txNew.vout[1].nValue -= masternodePayment;
         } else {
             txNew.vout.resize(2);
             txNew.vout[1].scriptPubKey = payee;
