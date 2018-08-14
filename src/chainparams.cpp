1506c8a5395fdf9dbe22a4b85c4f5bf010de4157
wagerrtor <tor@i2pmail.org>
2018-08-14 16:23:26 +0200

    Set testnet settings
    - nBlockFirstFraudulent = 21012
    - nBlockLastGoodCheckpoint = 350
    - nBlockEnforceInvalidUTXO = 350
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -301,9 +301,9 @@ public:
         nZerocoinStartTime = 1518696183; // GMT: Thursday, 15. February 2018 12:03:03
         nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = 1400; //Trigger a recalculation of accumulators
-        nBlockFirstFraudulent = 99999999; //First block that bad serials emerged (currently we do not have any) *** TODO ***
-        nBlockLastGoodCheckpoint = 1398; //Last valid accumulator checkpoint (currently we do not have any) *** TODO ***
-        nBlockEnforceInvalidUTXO = 1600; //Start enforcing the invalid UTXO's
+        nBlockFirstFraudulent = 21012; //First block that bad serials emerged (currently we do not have any) *** TODO ***
+        nBlockLastGoodCheckpoint = 350; //Last valid accumulator checkpoint (currently we do not have any) *** TODO ***
+        nBlockEnforceInvalidUTXO = 350; //Start enforcing the invalid UTXO's
         nInvalidAmountFiltered = 0; //Amount of invalid coins filtered through exchanges, that should be considered valid
         nBlockZerocoinV2 = 55000; //The block that zerocoin v2 becomes active
         nEnforceNewSporkKey = 1521604800; //!> Sporks signed after Wednesday, March 21, 2018 4:00:00 AM GMT must use the new spork key
