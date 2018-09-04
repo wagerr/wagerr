96d38b1e198c547675f1d2fd4b2256ff0a38bdec
Fuzzbawls <fuzzbawls@gmail.com>
2018-09-03 20:37:17 -0700

    Contrib: Update macdeployqtplus subdir

--- contrib/macdeploy/detached-sig-apply.sh
+++ contrib/macdeploy/detached-sig-apply.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+# Copyright (c) 2014-2015 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C
 set -e
 
-UNSIGNED=$1
-SIGNATURE=$2
+UNSIGNED="$1"
+SIGNATURE="$2"
 ARCH=x86_64
 ROOTDIR=dist
-BUNDLE=${ROOTDIR}/Wagerr-Qt.app
 TEMPDIR=signed.temp
 OUTDIR=signed-app
 
@@ -31,21 +35,21 @@ if [ -z "${CODESIGN_ALLOCATE}" ]; then
   CODESIGN_ALLOCATE=${TEMPDIR}/codesign_allocate
 fi
 
-for i in `find ${TEMPDIR} -name "*.sign"`; do
-  SIZE=`stat -c %s ${i}`
-  TARGET_FILE=`echo ${i} | sed 's/\.sign$//'`
+find ${TEMPDIR} -name "*.sign" | while read i; do
+  SIZE=`stat -c %s "${i}"`
+  TARGET_FILE="`echo "${i}" | sed 's/\.sign$//'`"
 
   echo "Allocating space for the signature of size ${SIZE} in ${TARGET_FILE}"
-  ${CODESIGN_ALLOCATE} -i ${TARGET_FILE} -a ${ARCH} ${SIZE} -o ${i}.tmp
+  ${CODESIGN_ALLOCATE} -i "${TARGET_FILE}" -a ${ARCH} ${SIZE} -o "${i}.tmp"
 
-  OFFSET=`${PAGESTUFF} ${i}.tmp -p | tail -2 | grep offset | sed 's/[^0-9]*//g'`
+  OFFSET=`${PAGESTUFF} "${i}.tmp" -p | tail -2 | grep offset | sed 's/[^0-9]*//g'`
   if [ -z ${QUIET} ]; then
     echo "Attaching signature at offset ${OFFSET}"
   fi
 
-  dd if=$i of=${i}.tmp bs=1 seek=${OFFSET} count=${SIZE} 2>/dev/null
-  mv ${i}.tmp ${TARGET_FILE}
-  rm ${i}
+  dd if="$i" of="${i}.tmp" bs=1 seek=${OFFSET} count=${SIZE} 2>/dev/null
+  mv "${i}.tmp" "${TARGET_FILE}"
+  rm "${i}"
   echo "Success."
 done
 mv ${TEMPDIR}/${ROOTDIR} ${OUTDIR}
