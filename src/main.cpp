521b25bb8e637d183617750c8250a8797a6269d9
Kokary <kokary@wagerr.com>
2018-09-11 23:08:12 +0200

    Increase minimum relay tx fee

--- src/main.cpp
+++ src/main.cpp
@@ -90,7 +90,7 @@ int64_t nReserveBalance = 0;
  * We are ~100 times smaller then bitcoin now (2015-06-23), set minRelayTxFee only 10 times higher
  * so it's still 10 times lower comparing to bitcoin.
  */
-CFeeRate minRelayTxFee = CFeeRate(10000);
+CFeeRate minRelayTxFee = CFeeRate(100000);
 
 CTxMemPool mempool(::minRelayTxFee);
 
