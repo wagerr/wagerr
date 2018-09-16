058943abb96323114626bd11dc299eec6831e1fe
Kokary <kokary@wagerr.com>
2018-09-16 19:58:21 +0200

    [WGRCORE-106] Add RPC call for spending zWGR with coin control

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -444,6 +444,7 @@ static const CRPCCommand vRPCCommands[] =
         {"zerocoin", "listzerocoinamounts", &listzerocoinamounts, false, false, true},
         {"zerocoin", "mintzerocoin", &mintzerocoin, false, false, true},
         {"zerocoin", "spendzerocoin", &spendzerocoin, false, false, true},
+        {"zerocoin", "spendzerocoinfrom", &spendzerocoinfrom, false, false, true},
         {"zerocoin", "resetmintzerocoin", &resetmintzerocoin, false, false, true},
         {"zerocoin", "resetspentzerocoin", &resetspentzerocoin, false, false, true},
         {"zerocoin", "getarchivedzerocoin", &getarchivedzerocoin, false, false, true},
