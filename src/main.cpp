4744ebc6ad8a81e5743e0ed8deeb367fa3b7b794
Fuzzbawls <fuzzbawls@gmail.com>
2018-09-18 21:38:19 -0700

    Main: reject non-final transactions using nLocktime from entering the mempool.
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -1324,6 +1324,12 @@ bool AcceptToMemoryPool(CTxMemPool& pool, CValidationState& state, const CTransa
         return state.DoS(100, error("AcceptToMemoryPool: coinstake as individual tx. txid=%s", tx.GetHash().GetHex()),
             REJECT_INVALID, "coinstake");
 
+    // Only accept nLockTime-using transactions that can be mined in the next
+    // block; we don't want our mempool filled up with transactions that can't
+    // be mined yet.
+    if (!CheckFinalTx(tx, STANDARD_LOCKTIME_VERIFY_FLAGS))
+        return state.DoS(0, false, REJECT_NONSTANDARD, "non-final");
+
     // Rather not work on nonstandard transactions (unless -testnet/-regtest)
     string reason;
     if (Params().RequireStandard() && !IsStandardTx(tx, reason))
