eons-finance/ALQO

Build: Include limits.h in bignum header
Some systems won't have CHAR_BIT defined without explicitely including
this.

src/libzerocoin/bignum.h
@@ -7,11 +7,12 @@
 #define BITCOIN_BIGNUM_H
 
 #if defined HAVE_CONFIG_H
-#include "pivx-config.h"
+#include "config/pivx-config.h"
 #endif
 
 #include <stdexcept>
 #include <vector>
+#include <limits.h>
 #if defined(USE_NUM_GMP)
 #include <gmp.h>
 #endif

wagerr/wagerr

src/libzerocoin/bignum.h
@@ -8,11 +8,12 @@
 #define BITCOIN_BIGNUM_H
 
 #if defined HAVE_CONFIG_H
-#include "wagerr-config.h"
+#include "config/wagerr-config.h"
 #endif
 
 #include <stdexcept>
 #include <vector>
+#include <limits.h>
 #if defined(USE_NUM_GMP)
 #include <gmp.h>
 #endif