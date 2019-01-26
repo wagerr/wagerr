9e3ae2c5dd3a087537dedda3c65a7340a766f195
furszy <5377650+furszy@users.noreply.github.com>
2019-01-26 22:29:22 +0100

    AcceptBlock(), serial double spend on the same block validation
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -4557,9 +4557,24 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         const bool isBlockFromFork = pindexPrev != nullptr && !chainActive.Contains(pindexPrev);
         CTransaction &stakeTxIn = block.vtx[1];
 
-        // Check validity of the coinStake.
-        if(!stakeTxIn.IsCoinStake())
-            return error("%s: no coin stake on vtx pos 1", __func__);
+        // ZC started after PoS.
+        // Check for serial double spent on the same block, TODO: Move this to the proper method..
+        if(nHeight >= Params().Zerocoin_StartHeight()) {
+            vector<CBigNum> inBlockSerials;
+            for (CTransaction tx : block.vtx) {
+                for (CTxIn in: tx.vin) {
+                    if (in.scriptSig.IsZerocoinSpend()) {
+                        CoinSpend spend = TxInToZerocoinSpend(in);
+                        // Check for serials double spending in the same block
+                        if (std::find(inBlockSerials.begin(), inBlockSerials.end(), spend.getCoinSerialNumber()) != inBlockSerials.end()) {
+                            return state.DoS(100, error("%s: serial double spent on the same block", __func__));
+                        }
+                        inBlockSerials.push_back(spend.getCoinSerialNumber());
+                    }
+                }
+            }
+            inBlockSerials.clear();
+        }
 
 
         // Check whether is a fork or not
@@ -4581,6 +4596,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             }
             const bool hasWGRInputs = !wgrInputs.empty();
             const bool hasZWGRInputs = !zWGRInputs.empty();
+
             vector<CBigNum> vBlockSerials;
             CBlock bl;
             // Go backwards on the forked chain up to the split
@@ -4596,20 +4612,18 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                         // Loop through every input of the staking tx
                         for (CTxIn stakeIn : wgrInputs) {
                             // if it's already spent
+
                             // First regular staking check
                             if(hasWGRInputs) {
                                 if (stakeIn.prevout == in.prevout) {
-                                    // reject the block
-                                    return state.DoS(100,
-                                                     error("%s: input already spent on a previous block",
-                                                           __func__));
+                                    return state.DoS(100, error("%s: input already spent on a previous block", __func__));
                                 }
                             }
+
                             // Second, if there is zPoS staking then store the serials for later check
                             if(hasZWGRInputs) {
                                 if(in.scriptSig.IsZerocoinSpend()){
-                                    CoinSpend spend = TxInToZerocoinSpend(in);
-                                    vBlockSerials.push_back(spend.getCoinSerialNumber());
+                                    vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
                                 }
                             }
                         }
