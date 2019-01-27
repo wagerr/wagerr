9b5da12fc1dac663dffc37754be130715c38fabb
Fuzzbawls <fuzzbawls@gmail.com>
2019-01-26 21:15:19 -0800

    Tests: Remove whitespace in default conf file
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -295,7 +295,7 @@ def initialize_datadir(dirname, n):
         f.write("port=" + str(p2p_port(n)) + "\n")
         f.write("rpcport=" + str(rpc_port(n)) + "\n")
         f.write("listenonion=0\n")
-        f.write("﻿litemode=1\n")
+        f.write("litemode=1\n")
         f.write("enablezeromint=0\n")
         f.write("staking=0\n")
         f.write("spendzeroconfchange=1\n")
