3fb991026cb51fcd31c3ba82c19b8642c396aaea
furszy <5377650+furszy@users.noreply.github.com>
2019-01-30 20:53:24 +0100

    AcceptBlock() - not accept blocks from a forked chain that exceed the max reorg limit
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -4597,15 +4597,22 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             const bool hasWGRInputs = !wgrInputs.empty();
             const bool hasZWGRInputs = !zWGRInputs.empty();
 
+            int readBlock = 0;
             vector<CBigNum> vBlockSerials;
             CBlock bl;
             // Go backwards on the forked chain up to the split
             do {
+                // Check if the forked chain is longer than the max reorg limit
+                if(readBlock == Params().MaxReorganizationDepth()){
+                    // TODO: Remove this chain from disk.
+                    return error("%s: forked chain longer than maximum reorg limit", __func__);
+                }
+
                 if(!ReadBlockFromDisk(bl, prev))
                     // Previous block not on disk
                     return error("%s: previous block %s not on disk", __func__, prev->GetBlockHash().GetHex());
-
-
+                // Increase amount of read blocks
+                readBlock++;
                 // Loop through every input from said block
                 for (CTransaction t : bl.vtx) {
                     for (CTxIn in: t.vin) {
