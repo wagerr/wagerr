1f4bbbb3e1f15b973fc2ac9cdff71c4b3c69920f
random-zebra <random.zebra@protonmail.com>
2019-02-06 13:51:54 +0100

    wagerr_fake_stake Test05 - more blocks fix
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- test/functional/fake_stake/test05.py
+++ test/functional/fake_stake/test05.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from random import randint
 import time
 
 from test_framework.authproxy import JSONRPCException
@@ -19,7 +19,7 @@ class Test_05(WAGERR_FakeStakeTest):
     def run_test(self):
         self.init_test()
         INITAL_MINED_BLOCKS = 300
-        self.NUM_BLOCKS = 10
+        self.NUM_BLOCKS = 3
 
         # 1) Starting mining blocks
         self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
@@ -38,38 +38,51 @@ class Test_05(WAGERR_FakeStakeTest):
             block_count = self.node.getblockcount()
             pastBlockHash = self.node.getblockhash(block_count)
             block = self.create_spam_block(pastBlockHash, stakingPrevOuts, block_count + 1, True)
-            self.log.info("Sending block %d", block_count + 1)
+            self.log.info("Sending block %d [%s...]", block_count + 1, block.hash[:7])
             var = self.node.submitblock(bytes_to_hex_str(block.serialize()))
             assert_equal(var, None)
-            self.log_data_dir_size()
 
             try:
                 block_ret = self.node.getblock(block.hash)
                 if block_ret is not None:
                     raise AssertionError("Error, block stored in main chain")
-            except JSONRPCException as error:
-                self.log.info(error)
+            except JSONRPCException as e:
+                err_msg = str(e)
+                if err_msg == "Can't read block from disk (-32603)":
+                    err_msg = "Good. Block was not stored on disk."
+                self.log.info(err_msg)
+
+            # remove a random prevout from the list (to randomize block creation)
+            stakingPrevOuts.popitem()
 
         self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.log_data_dir_size()
         time.sleep(3)
 
         # 4) Spam Blocks on a forked chain
+        # regenerate prevouts
+        self.log.info("Mining %d more blocks as buffer..." % 31)
+        self.node.generate(31)
+        stakingPrevOuts = self.get_prevouts(utxo_list)
         for i in range(0, self.NUM_BLOCKS):
             if i !=0:
                 self.log.info("Sent %s blocks out of %s" % (str(i), str(self.NUM_BLOCKS)))
-            block_count = self.node.getblockcount() - 20
-            pastBlockHash = self.node.getblockhash(block_count)
-            block = self.create_spam_block(pastBlockHash, stakingPrevOuts, block_count + 1, True)
-            self.log.info("Sending block %d", block_count + 1)
+            randomCount = randint(block_count - 30, block_count)
+            pastBlockHash = self.node.getblockhash(randomCount)
+            block = self.create_spam_block(pastBlockHash, stakingPrevOuts, randomCount + 1, True)
+            self.log.info("Sending block %d [%s...]", randomCount + 1, block.hash[:7])
             var = self.node.submitblock(bytes_to_hex_str(block.serialize()))
             assert_equal(var, None)
-            self.log_data_dir_size()
 
             try:
                 block_ret = self.node.getblock(block.hash)
                 if block_ret is not None:
                     raise AssertionError("Error, block stored in forked chain")
-            except JSONRPCException as error:
-                self.log.info(error)
+            except JSONRPCException as e:
+                err_msg = str(e)
+                if err_msg == "Can't read block from disk (-32603)":
+                    err_msg = "Good. Block was not stored on disk."
+                self.log.info(err_msg)
 
         self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.log_data_dir_size()
