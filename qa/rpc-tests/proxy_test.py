050528ae15383c5df7a4a83ed6a0a0db8909d014
wagerrtor <tor@i2pmail.org>
2019-02-11 22:30:50 +0100

    Fix conflicts for 22bfe7ea

--- qa/rpc-tests/proxy_test.py
+++ qa/rpc-tests/proxy_test.py
@@ -100,12 +100,12 @@ class ProxyTest(BitcoinTestFramework):
         rv.append(cmd)
 
         # Test: outgoing onion connection through node
-        node.addnode("wagerrsomeaddr.onion:55002", "onetry")
+        node.addnode("wagerrvj7kcklujarx.onion:51472", "onetry")
         cmd = proxies[2].queue.get()
         assert(isinstance(cmd, Socks5Command))
         assert_equal(cmd.atyp, AddressType.DOMAINNAME)
-        assert_equal(cmd.addr, "wagerrsomeaddr.onion")
-        assert_equal(cmd.port, 55002)
+        assert_equal(cmd.addr, "wagerrvj7kcklujarx.onion")
+        assert_equal(cmd.port, 51472)
         if not auth:
             assert_equal(cmd.username, None)
             assert_equal(cmd.password, None)
