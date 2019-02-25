0507124dfbfdd56c01b7f82bc0926533377b30c4
Fuzzbawls <fuzzbawls@gmail.com>
2019-02-25 00:58:04 -0800

    Fixup signrawtransaction on regtest
    The `signrawtransaction` RPC command needed some modification to appease
     the fake stake test cases, however that change introduced a minor
     inconsistency in it's operation.
    
    This PR restores the original functionality, while still providing a
    means to be used for the fake stake test cases.
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -772,7 +772,7 @@ UniValue signrawtransaction(const UniValue& params, bool fHelp)
         CTxIn& txin = mergedTx.vin[i];
         const CCoins* coins = view.AccessCoins(txin.prevout.hash);
         if (Params().NetworkID() == CBaseChainParams::REGTEST) {
-            if (mapPrevOut.count(txin.prevout) == 0)
+            if (mapPrevOut.count(txin.prevout) == 0 && (coins == NULL || !coins->IsAvailable(txin.prevout.n)))
             {
                 TxInErrorToJSON(txin, vErrors, "Input not found");
                 continue;
@@ -783,7 +783,7 @@ UniValue signrawtransaction(const UniValue& params, bool fHelp)
                 continue;
             }
         }
-        const CScript& prevPubKey = (Params().NetworkID() == CBaseChainParams::REGTEST ? mapPrevOut[txin.prevout] : coins->vout[txin.prevout.n].scriptPubKey);
+        const CScript& prevPubKey = (Params().NetworkID() == CBaseChainParams::REGTEST && mapPrevOut.count(txin.prevout) != 0 ? mapPrevOut[txin.prevout] : coins->vout[txin.prevout.n].scriptPubKey);
 
         txin.scriptSig.clear();
         // Only sign SIGHASH_SINGLE if there's a corresponding output:
