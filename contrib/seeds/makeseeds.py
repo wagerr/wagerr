34bc9cb256ebfebea63773c6f6a4b7dc347f118b
wagerrtor <tor@i2pmail.org>
2019-03-22 23:11:40 +0100

    Bump version to development 3.0.99
    - Bump shortcuts in snap
    - use 2019_regtest-mode-and-fake-test-suite to build snap for edge channel
    - Update makeseeds script to ver 3
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -26,7 +26,7 @@ SUSPICIOUS_HOSTS = {
 PATTERN_IPV4 = re.compile(r"^((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})):(\d+)$")
 PATTERN_IPV6 = re.compile(r"^\[([0-9a-z:]+)\]:(\d+)$")
 PATTERN_ONION = re.compile(r"^([abcdefghijklmnopqrstuvwxyz234567]{16}\.onion):(\d+)$")
-PATTERN_AGENT = re.compile(r"^(/Wagerr Core:2.0.(0|99)/)$")
+PATTERN_AGENT = re.compile(r"^(/Wagerr Core:3.0.(0|99)/)$")
 
 def parseline(line):
     sline = line.split()
