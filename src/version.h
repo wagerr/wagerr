fcc3638c388bc2fa4b083ca3dc2d0a7656acc5df
wagerrtor <tor@i2pmail.org>
2019-03-23 04:12:42 +0100

    Update MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT
    Info : If MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMEN is not set by time, then the
    result will be that only first block will be accepted and sync would be
    only possible with bootstrap/existing blocks folder
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@ static const int INIT_PROTO_VERSION = 214;
 static const int GETHEADERS_VERSION = 70912;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70925; //version 1.5.0
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70923; //version 2.x
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70925;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
