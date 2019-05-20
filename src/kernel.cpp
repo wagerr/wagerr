0f92ff7272f4c51dbd17ff0d97f470fdeb0a651b
warrows <warrows@gamer666.fr>
2019-05-20 12:46:51 +0200

    Guard time checks changes
    Guard these changes against the protocol version change enforcement
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -433,7 +433,7 @@ bool CheckProofOfStake(const CBlock block, uint256& hashProofOfStake, std::uniqu
 
     unsigned int nBlockFromTime = blockfrom.nTime;
     unsigned int nTxTime = block.nTime;
-    if (!txin.IsZerocoinSpend()) { //Equivalent for zWGR is checked above in ContextualCheckZerocoinStake()
+    if (!txin.IsZerocoinSpend() && nPreviousBlockHeight >= Params().Zerocoin_Block_Public_Spend_Enabled() - 1) { //Equivalent for zWGR is checked above in ContextualCheckZerocoinStake()
         if (nTxTime < nBlockFromTime) // Transaction timestamp nTxTime
             return error("CheckStakeKernelHash() : nTime violation - nBlockFromTime=%d nTimeTx=%d", nBlockFromTime, nTxTime);
         if (nBlockFromTime + nStakeMinAge > nTxTime) // Min age requirement
