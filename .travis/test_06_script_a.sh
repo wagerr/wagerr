1207cae94858859e0eccf4926c7a80582dbc5685
Fuzzbawls <fuzzbawls@gmail.com>
2019-06-09 16:18:27 -0700

    Update TravisCI from upstream
    Most notable change here is the introduction of two breakpoints that
    will store the current cache and stop if the previous step took a long
    time. This is intended to avoid hitting the global time limit of 50
    minutes for builds that would need to rebuild the depends cache.
    
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- .travis/test_06_script_a.sh
+++ .travis/test_06_script_a.sh
@@ -40,28 +40,11 @@ BEGIN_FOLD configure
 DOCKER_EXEC ./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)
 END_FOLD
 
+set -o errtrace
+trap 'DOCKER_EXEC "cat ${TRAVIS_BUILD_DIR}/sanitizer-output/* 2> /dev/null"' ERR
+
 BEGIN_FOLD build
 DOCKER_EXEC make $MAKEJOBS $GOAL || ( echo "Build failure. Verbose build follows." && DOCKER_EXEC make $GOAL V=1 ; false )
 END_FOLD
 
-if [ "$RUN_UNIT_TESTS" = "true" ]; then
-  BEGIN_FOLD unit-tests
-  DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1
-  END_FOLD
-fi
-
-if [ "$RUN_BENCH" = "true" ]; then
-  BEGIN_FOLD bench
-  DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib $OUTDIR/bin/bench_wagerr -scaling=0.001
-  END_FOLD
-fi
-
-if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
-  extended="--extended --exclude feature_pruning,feature_dbcrash"
-fi
-
-if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
-  BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 --coverage --quiet --failfast ${extended}
-  END_FOLD
-fi
+cd ${TRAVIS_BUILD_DIR} || (echo "could not enter travis build dir $TRAVIS_BUILD_DIR"; exit 1)
