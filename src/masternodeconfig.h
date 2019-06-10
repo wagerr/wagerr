8a4a6a5c963f1829427d79265fde03138c0eabc4
furszy <5377650+furszy@users.noreply.github.com>
2019-06-11 01:55:15 +0200

    Delete single master node implemented.

--- src/masternodeconfig.h
+++ src/masternodeconfig.h
@@ -99,6 +99,7 @@ public:
     void clear();
     bool read(std::string& strErr);
     void add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex);
+    void remove(std::string alias);
 
     std::vector<CMasternodeEntry>& getEntries()
     {
