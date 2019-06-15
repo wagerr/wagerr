9bb16ee02796b9aa7d0c011d2b03cdb8ce4a7aae
furszy <matiasfurszyfer@protonmail.com>
2019-06-16 01:43:42 +0200

    Max tip age to consider a node in IBD status customizable. Backport from bitcoin##7208.
    Signed-off-by: wagerrtor <tor@i2pmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -83,6 +83,9 @@ unsigned int nCoinCacheSize = 5000;
 bool fAlerts = DEFAULT_ALERTS;
 bool fClearSpendCache = false;
 
+/* If the tip is older than this (in seconds), the node is considered to be in initial block download. */
+int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
+
 unsigned int nStakeMinAge = 60 * 60;
 int64_t nReserveBalance = 0;
 
