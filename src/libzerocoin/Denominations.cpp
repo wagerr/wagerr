73f71bfab46ba709cce88919894634e3b8cec67e
wagerrtor <tor@i2pmail.org>
2019-06-16 18:47:04 +0200

    fix and update libzerocoin

--- src/libzerocoin/Denominations.cpp
+++ src/libzerocoin/Denominations.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018 The WAGERR developers
+// Copyright (c) 2017-2018 The PIVX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -60,7 +60,7 @@ CoinDenomination AmountToZerocoinDenomination(CAmount amount)
 }
 
 // return the highest denomination that is less than or equal to the amount given
-// use case: converting WGR to zWGR without user worrying about denomination math themselves
+// use case: converting PIV to zPIV without user worrying about denomination math themselves
 CoinDenomination AmountToClosestDenomination(CAmount nAmount, CAmount& nRemaining)
 {
     if (nAmount < 1 * COIN)
