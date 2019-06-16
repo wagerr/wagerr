2747b6e75df1f489cb3bed720749e7b35218264f
wagerrtor <tor@i2pmail.org>
2019-06-16 18:47:04 +0200

    fix and update libzerocoin

--- src/libzerocoin/CoinSpend.h
+++ src/libzerocoin/CoinSpend.h
@@ -9,7 +9,7 @@
  * @copyright  Copyright 2013 Ian Miers, Christina Garman and Matthew Green
  * @license    This project is released under the MIT license.
  **/
-// Copyright (c) 2017-2018 The WAGERR developers
+// Copyright (c) 2017-2018 The PIVX developers
 
 #ifndef COINSPEND_H_
 #define COINSPEND_H_
@@ -41,7 +41,7 @@ public:
     CoinSpend(){};
 
     //! \param paramsV1 - if this is a V1 zerocoin, then use params that existed with initial modulus, ignored otherwise
-    //! \param paramsV2 - params that begin when V2 zerocoins begin on the WAGERR network
+    //! \param paramsV2 - params that begin when V2 zerocoins begin on the PIVX network
     //! \param strm - a serialized CoinSpend
     template <typename Stream>
     CoinSpend(const ZerocoinParams* paramsV1, const ZerocoinParams* paramsV2, Stream& strm) :
