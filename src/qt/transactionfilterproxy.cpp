944b52258144cf32d407c54cc57b93193394d3a3
wagerrtor <tor@i2pmail.org>
2019-06-18 23:59:00 +0200

    fix wrong naming of zWGR

--- src/qt/transactionfilterproxy.cpp
+++ src/qt/transactionfilterproxy.cpp
@@ -123,6 +123,6 @@ int TransactionFilterProxy::rowCount(const QModelIndex& parent) const
 bool TransactionFilterProxy::isOrphan(const int status, const int type)
 {
     return ( (type == TransactionRecord::Generated || type == TransactionRecord::StakeMint ||
-            type == TransactionRecord::StakeZPIV || type == TransactionRecord::MNReward)
+            type == TransactionRecord::StakeZWGR || type == TransactionRecord::MNReward)
             && (status == TransactionStatus::Conflicted || status == TransactionStatus::NotAccepted) );
 }
