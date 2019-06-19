a003b4d1273e4cf040f029d1dfaad2ca6a63f7da
wagerrtor <tor@i2pmail.org>
2019-06-19 04:36:57 +0200

    update gen-manpages

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -1,26 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+export LC_ALL=C
 TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
-SRCDIR=${SRCDIR:-$TOPDIR/src}
+BUILDDIR=${BUILDDIR:-$TOPDIR}
+
+BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-WAGERRD=${WAGERRD:-$SRCDIR/wagerrd}
-WAGERRCLI=${WAGERRCLI:-$SRCDIR/wagerr-cli}
-WAGERRTX=${WAGERRTX:-$SRCDIR/wagerr-tx}
-WAGERRQT=${WAGERRQT:-$SRCDIR/qt/wagerr-qt}
+BITCOIND=${BITCOIND:-$BINDIR/wagerrd}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/wagerr-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/wagerr-tx}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/wagerr-qt}
 
-[ ! -x $WAGERRD ] && echo "$WAGERRD not found or not executable." && exit 1
+[ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVER=($($WAGERRCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+BTCVER=($($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for wagerrd if --version-string is not set,
-# but has different outcomes for wagerr-qt and wagerr-cli.
+# This gets autodetected fine for bitcoind if --version-string is not set,
+# but has different outcomes for bitcoin-qt and bitcoin-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$WAGERRD --version | sed -n '1!p' >> footer.h2m
+$BITCOIND --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $WAGERRD $WAGERRCLI $WAGERRTX $WAGERRQT; do
+for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $BITCOINQT; do
   cmdname="${cmd##*/}"
   help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
   sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
