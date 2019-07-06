3cd09dba6c3266fe39d784751d6e686aa83ad8c3
warrows <warrows@gamer666.fr>
2019-07-06 11:04:43 +0200

    scripted-diff: use insecure_rand256/randrange more
    -BEGIN VERIFY SCRIPT-
    sed -i "s/\<GetRandHash(/insecure_rand256(/" src/test/*_tests.cpp
    sed -i "s/\<GetRand(/insecure_randrange(/" src/test/*_tests.cpp
    src/test/test_bitcoin.cpp
    sed -i 's/\<insecure_rand() % \([0-9]\+\)/insecure_randrange(\1)/g'
    src/test/*_tests.cpp
    -END VERIFY SCRIPT-

--- src/test/test_wagerr.cpp
+++ src/test/test_wagerr.cpp
@@ -50,7 +50,7 @@ TestingSetup::TestingSetup()
         bitdb.MakeMock();
 #endif
         ClearDatadirCache();
-        pathTemp = GetTempPath() / strprintf("test_wagerr_%lu_%i", (unsigned long)GetTime(), (int)(GetRand(100000)));
+        pathTemp = GetTempPath() / strprintf("test_wagerr_%lu_%i", (unsigned long)GetTime(), (int)(insecure_randrange(100000)));
         boost::filesystem::create_directories(pathTemp);
         mapArgs["-datadir"] = pathTemp.string();
         pblocktree = new CBlockTreeDB(1 << 20, true);
