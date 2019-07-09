55f5373cbe38d55a281307693226706df9b8b82a
warrows <warrows@gamer666.fr>
2019-07-09 09:45:27 +0200

    Log more info

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -16,6 +16,6 @@ fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 ${TEST_RUNNER_EXTRA} --quiet
+  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 ${TEST_RUNNER_EXTRA}
   END_FOLD
 fi
