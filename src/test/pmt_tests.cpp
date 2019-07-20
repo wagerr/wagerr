f911b183709aa3dc9f7eec4c0686a83db841004b
warrows <warrows@gamer666.fr>
2019-07-20 10:58:13 +0200

    Do not store Merkle branches in the wallet
    Backport of https://github.com/bitcoin/bitcoin/pull/6550
    
    Assume that when a wallet transaction has a valid block hash and
    transaction position
    in it, the transaction is actually there. We're already trusting wallet
    data in a
    much more fundamental way anyway.
    
    To prevent backward compatibility issues, a new record is used for
    storing the
    block locator in the wallet. Old wallets will see a wallet file
    synchronized up
    to the genesis block, and rescan automatically.

--- src/test/pmt_tests.cpp
+++ src/test/pmt_tests.cpp
@@ -46,7 +46,7 @@ BOOST_AUTO_TEST_CASE(pmt_test1)
         }
 
         // calculate actual merkle root and height
-        uint256 merkleRoot1 = block.BuildMerkleTree();
+        uint256 merkleRoot1 = block.ComputeMerkleRoot();
         std::vector<uint256> vTxid(nTx, 0);
         for (unsigned int j=0; j<nTx; j++)
             vTxid[j] = block.vtx[j].GetHash();
--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -198,12 +198,14 @@ bool CWalletDB::EraseMultiSig(const CScript& dest)
 bool CWalletDB::WriteBestBlock(const CBlockLocator& locator)
 {
     nWalletDBUpdated++;
-    return Write(std::string("bestblock"), locator);
+    Write(std::string("bestblock"), CBlockLocator()); // Write empty block locator so versions that require a merkle branch automatically rescan
+    return Write(std::string("bestblock_nomerkle"), locator);
 }
 
 bool CWalletDB::ReadBestBlock(CBlockLocator& locator)
 {
-    return Read(std::string("bestblock"), locator);
+    if (Read(std::string("bestblock"), locator) && !locator.vHave.empty()) return true;
+    return Read(std::string("bestblock_nomerkle"), locator);
 }
 
 bool CWalletDB::WriteOrderPosNext(int64_t nOrderPosNext)
