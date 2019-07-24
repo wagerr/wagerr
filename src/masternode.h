95e15f9d86843023e2bdaebaae44ce8ee3020c59
furszy <matiasfurszyfer@protonmail.com>
2019-07-24 16:41:21 -0300

    Master node MISSING status added, only happens when the Master Node has been created on the controller side, added to the masternode.conf and not started. It's not added to the vMasternodes list or any other network list, only lives locally on the masternodeConfig.

--- src/masternode.h
+++ src/masternode.h
@@ -121,7 +121,8 @@ public:
         MASTERNODE_WATCHDOG_EXPIRED,
         MASTERNODE_POSE_BAN,
         MASTERNODE_VIN_SPENT,
-        MASTERNODE_POS_ERROR
+        MASTERNODE_POS_ERROR,
+        MASTERNODE_MISSING
     };
 
     CTxIn vin;
@@ -277,6 +278,7 @@ public:
         if (activeState == CMasternode::MASTERNODE_VIN_SPENT) strStatus = "VIN_SPENT";
         if (activeState == CMasternode::MASTERNODE_REMOVE) strStatus = "REMOVE";
         if (activeState == CMasternode::MASTERNODE_POS_ERROR) strStatus = "POS_ERROR";
+        if (activeState == CMasternode::MASTERNODE_MISSING) strStatus = "MISSING";
 
         return strStatus;
     }
