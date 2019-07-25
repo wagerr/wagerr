f4ca87f376de46ee086e3c1a5f34c4abc18b8074
furszy <matiasfurszyfer@protonmail.com>
2019-07-25 20:28:28 -0300

    Badly nStakeSplitThreshold set in optionsModel fixed + value moved to constant STAKE_SPLIT_THRESHOLD.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -197,6 +197,9 @@ private:
     void SyncMetaData(std::pair<TxSpends::iterator, TxSpends::iterator>);
 
 public:
+
+    static const int STAKE_SPLIT_THRESHOLD = 2000;
+
     bool MintableCoins();
     bool SelectStakeCoins(std::list<std::unique_ptr<CStakeInput> >& listInputs, CAmount nTargetAmount, int blockHeight, bool fPrecompute = false);
     bool IsCollateralAmount(CAmount nInputAmount) const;
@@ -338,7 +341,7 @@ public:
 
         // Stake Settings
         nHashDrift = 45;
-        nStakeSplitThreshold = 2000;
+        nStakeSplitThreshold = STAKE_SPLIT_THRESHOLD;
         nHashInterval = 22;
         nStakeSetUpdateTime = 300; // 5 minutes
 
