57e91f0200f3ffed10f936b109bdd7e86a21455e
random-zebra <random.zebra@protonmail.com>
2019-09-25 01:00:33 +0200

    Placeholder block height for activation of new signatures

--- src/chainparams.h
+++ src/chainparams.h
@@ -145,6 +145,7 @@ public:
     int Zerocoin_Block_V2_Start() const { return nBlockZerocoinV2; }
     int BIP65Height() const { return nBIP65Height; }
     bool IsStakeModifierV2(const int nHeight) const { return nHeight >= nBlockStakeModifierlV2; }
+    int NewSigsActive(const int nHeight) const { return nHeight >= nBlockEnforceNewMessageSignatures; }
 
     // fake serial attack
     int Zerocoin_Block_EndFakeSerial() const { return nFakeSerialBlockheightEnd; }
@@ -252,6 +253,7 @@ protected:
     int nPublicZCSpends;
     int nBIP65Height;
     int nBlockStakeModifierlV2;
+    int nBlockEnforceNewMessageSignatures;
 
     // fake serial attack
     int nFakeSerialBlockheightEnd = 0;
