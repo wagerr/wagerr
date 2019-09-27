00b6232de6e019f837ea528e0e1039af1ac3dfec
random-zebra <random.zebra@protonmail.com>
2019-09-27 21:07:48 +0200

    define CSignedMessage parent class
    New class inherited by all signed messages with unque
    Sign()/CheckSignature() functions. Child classes now need to define
    individually only the implementation of GetStrMessage() and
    GetSignatureHash().

--- src/messagesigner.h
+++ src/messagesigner.h
@@ -20,11 +20,13 @@ class CMessageSigner
 public:
     /// Set the private/public key values, returns true if successful
     static bool GetKeysFromSecret(const std::string& strSecret, CKey& keyRet, CPubKey& pubkeyRet);
+    /// Get the hash based on the input message
+    static uint256 GetMessageHash(const std::string& strMessage);
     /// Sign the message, returns true if successful
     static bool SignMessage(const std::string& strMessage, std::vector<unsigned char>& vchSigRet, const CKey& key);
-    /// Verify the message signature, returns true if succcessful
+    /// Verify the message signature, returns true if successful
     static bool VerifyMessage(const CPubKey& pubkey, const std::vector<unsigned char>& vchSig, const std::string& strMessage, std::string& strErrorRet);
-    /// Verify the message signature, returns true if succcessful
+    /// Verify the message signature, returns true if successful
     static bool VerifyMessage(const CKeyID& keyID, const std::vector<unsigned char>& vchSig, const std::string& strMessage, std::string& strErrorRet);
 };
 
@@ -35,10 +37,50 @@ class CHashSigner
 public:
     /// Sign the hash, returns true if successful
     static bool SignHash(const uint256& hash, const CKey& key, std::vector<unsigned char>& vchSigRet);
-    /// Verify the hash signature, returns true if succcessful
+    /// Verify the hash signature, returns true if successful
     static bool VerifyHash(const uint256& hash, const CPubKey& pubkey, const std::vector<unsigned char>& vchSig, std::string& strErrorRet);
-    /// Verify the hash signature, returns true if succcessful
+    /// Verify the hash signature, returns true if successful
     static bool VerifyHash(const uint256& hash, const CKeyID& keyID, const std::vector<unsigned char>& vchSig, std::string& strErrorRet);
+};
+
+/** Base Class for all signed messages on the network
+ */
+class CSignedMessage
+{
+protected:
+    std::vector<unsigned char> vchSig;
+    void swap(CSignedMessage& first, CSignedMessage& second); // Swap two messages
+
+public:
+    int nMessVersion;
+
+    CSignedMessage() :
+        vchSig(),
+        nMessVersion(MessageVersion::MESS_VER_HASH)
+    {}
+    CSignedMessage(const CSignedMessage& other)
+    {
+        vchSig = other.GetVchSig();
+        nMessVersion = other.nMessVersion;
+    }
+    virtual ~CSignedMessage() {};
+
+    // Sign-Verify message
+    bool Sign(const CKey& key, const CPubKey& pubKey, const bool fNewSigs);
+    bool Sign(const std::string strSignKey, const bool fNewSigs);
+    bool CheckSignature(const CPubKey& pubKey) const;
+    bool CheckSignature(const bool fSignatureCheck = true) const;
+
+    // Pure virtual functions (used in Sign-Verify functions)
+    // Must be implemented in child classes
+    virtual uint256 GetSignatureHash() const = 0;
+    virtual std::string GetStrMessage() const = 0;
+    virtual const CPubKey* GetPublicKey(std::string& strErrorRet) const = 0;
+
+    // Setters and getters
+    void SetVchSig(const std::vector<unsigned char>& vchSigIn) { vchSig = vchSigIn; }
+    std::vector<unsigned char> GetVchSig() const { return vchSig; }
+    std::string GetSignatureBase64() const;
 };
 
 #endif
