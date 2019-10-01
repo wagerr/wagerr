3321f57054ae4f8fe6cd45e7262dbab31d328f14
random-zebra <random.zebra@protonmail.com>
2019-10-01 23:55:44 +0200

    better log for CHashSigner::VerifyHash failures

--- src/messagesigner.cpp
+++ src/messagesigner.cpp
@@ -66,8 +66,8 @@ bool CHashSigner::VerifyHash(const uint256& hash, const CKeyID& keyID, const std
 
     if(pubkeyFromSig.GetID() != keyID) {
         strErrorRet = strprintf("Keys don't match: pubkey=%s, pubkeyFromSig=%s, hash=%s, vchSig=%s",
-                    keyID.ToString(), pubkeyFromSig.GetID().ToString(), hash.ToString(),
-                    EncodeBase64(&vchSig[0], vchSig.size()));
+                CBitcoinAddress(keyID).ToString(), CBitcoinAddress(pubkeyFromSig.GetID()).ToString(),
+                hash.ToString(), EncodeBase64(&vchSig[0], vchSig.size()));
         return false;
     }
 
