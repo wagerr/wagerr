4da08b9ef9d05ffb72a07207f91eafd94bf5599e
furszy <matiasfurszyfer@protonmail.com>
2019-10-10 17:59:46 -0300

    Do not look for address label if the input address is empty

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -473,12 +473,15 @@ bool AddressTableModel::removeRows(int row, int count, const QModelIndex& parent
  */
 QString AddressTableModel::labelForAddress(const QString& address) const
 {
-    {
-        LOCK(wallet->cs_wallet);
-        CBitcoinAddress address_parsed(address.toStdString());
-        std::map<CTxDestination, CAddressBookData>::iterator mi = wallet->mapAddressBook.find(address_parsed.Get());
-        if (mi != wallet->mapAddressBook.end()) {
-            return QString::fromStdString(mi->second.name);
+    // TODO: Check why do we have empty addresses..
+    if (!address.isEmpty()) {
+        {
+            LOCK(wallet->cs_wallet);
+            CBitcoinAddress address_parsed(address.toStdString());
+            std::map<CTxDestination, CAddressBookData>::iterator mi = wallet->mapAddressBook.find(address_parsed.Get());
+            if (mi != wallet->mapAddressBook.end()) {
+                return QString::fromStdString(mi->second.name);
+            }
         }
     }
     return QString();
