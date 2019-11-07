d02563e3ca372f0d929cc4214d81adcf13381604
random-zebra <random.zebra@protonmail.com>
2019-11-07 17:36:06 +0100

    Fix fake-stake spent serials detection on forked chains

--- src/main.cpp
+++ src/main.cpp
@@ -4746,14 +4746,14 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     }
                 }
                 if(tx.IsCoinStake()) continue;
-                if(hasWGRInputs)
+                if(hasWGRInputs) {
                     // Check if coinstake input is double spent inside the same block
-                    for (const CTxIn& wgrIn : wgrInputs){
-                        if(wgrIn.prevout == in.prevout){
+                    for (const CTxIn& wgrIn : wgrInputs)
+                        if(wgrIn.prevout == in.prevout)
                             // double spent coinstake input inside block
                             return error("%s: double spent coinstake input inside block", __func__);
-                        }
-                    }
+                }
+
             }
         }
         inBlockSerials.clear();
@@ -4782,25 +4782,21 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     return error("%s: forked chain longer than maximum reorg limit", __func__);
                 }
 
-                // Loop through every input from said block
-                for (const CTransaction &t : bl.vtx) {
-                    for (const CTxIn &in: t.vin) {
+                // Loop through every tx of this block
+                for (const CTransaction& t : bl.vtx) {
+                    // Loop through every input of this tx
+                    for (const CTxIn& in: t.vin) {
+                        // If this input is a zerocoin spend, and the coinstake has zerocoin inputs
+                        // then store the serials for later check
+                        if(hasZWGRInputs && in.IsZerocoinSpend())
+                            vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
+
                         // Loop through every input of the staking tx
-                        for (const CTxIn &stakeIn : wgrInputs) {
-                            // if it's already spent
-
-                            // First regular staking check
-                            if (hasWGRInputs) {
-                                if (stakeIn.prevout == in.prevout) {
-                                    return state.DoS(100, error("%s: input already spent on a previous block",
-                                                                __func__));
-                                }
-
-                                // Second, if there is zPoS staking then store the serials for later check
-                                if(in.IsZerocoinSpend()){
-                                    vBlockSerials.push_back(TxInToZerocoinSpend(in).getCoinSerialNumber());
-                                }
-                            }
+                        if (hasWGRInputs) {
+                            for (const CTxIn& stakeIn : wgrInputs)
+                                // check if the tx input is double spending any coinstake input
+                                if (stakeIn.prevout == in.prevout)
+                                    return state.DoS(100, error("%s: input already spent on a previous block", __func__));
                         }
                     }
                 }
@@ -4817,7 +4813,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             splitHeight = prev->nHeight;
 
             // Now that this loop if completed. Check if we have zWGR inputs.
-            if(hasZWGRInputs){
+            if(hasZWGRInputs) {
                 for (const CTxIn& zWgrInput : zWGRInputs) {
                     libzerocoin::CoinSpend spend = TxInToZerocoinSpend(zWgrInput);
 
@@ -4831,7 +4827,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
 
                     // Now check if the serial exists before the chain split.
                     int nHeightTx = 0;
-                    if (IsSerialInBlockchain(spend.getCoinSerialNumber(), nHeightTx)){
+                    if (IsSerialInBlockchain(spend.getCoinSerialNumber(), nHeightTx)) {
                         // if the height is nHeightTx > chainSplit means that the spent occurred after the chain split
                         if(nHeightTx <= splitHeight)
                             return state.DoS(100, error("%s: serial double spent on main chain", __func__));
