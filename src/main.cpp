2a486517771bc0437fd05f5a0cb91f546e45ed94
random-zebra <random.zebra@protonmail.com>
2019-11-20 18:11:31 +0100

    reduce possible nTimeOffset to 15 seconds

--- src/main.cpp
+++ src/main.cpp
@@ -6197,8 +6197,6 @@ bool static ProcessMessage(CNode* pfrom, std::string strCommand, CDataStream& vR
                 item.second.RelayTo(pfrom);
         }
 
-        pfrom->fSuccessfullyConnected = true;
-
         std::string remoteAddr;
         if (fLogIPs)
             remoteAddr = ", peeraddr=" + pfrom->addr.ToString();
@@ -6210,7 +6208,13 @@ bool static ProcessMessage(CNode* pfrom, std::string strCommand, CDataStream& vR
 
         int64_t nTimeOffset = nTime - GetTime();
         pfrom->nTimeOffset = nTimeOffset;
-        AddTimeData(pfrom->addr, nTimeOffset);
+        if (nTimeOffset < 2 * Params().TimeSlotLength()) {
+            pfrom->fSuccessfullyConnected = true;
+            AddTimeData(pfrom->addr, nTimeOffset);
+        } else {
+            LogPrintf("timeOffset (%d seconds) too large. Disconnecting node %s\n", nTimeOffset, pfrom->addr.ToString());
+            pfrom->fDisconnect = true;
+        }
     }
 
 
