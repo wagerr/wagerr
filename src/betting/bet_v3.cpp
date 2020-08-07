a02afdeb00a89f0eaff97f9c0ae75d46e590cda8
wagerrdeveloper <lenny@wagerr.com>
2020-08-07 15:02:16 +0700

    Probably raise exception on block height 1092935 on linux debug build

--- src/betting/bet_v3.cpp
+++ src/betting/bet_v3.cpp
@@ -136,7 +136,7 @@ void GetPLBetPayoutsV3(CBettingsView &bettingsViewCache, const int nNewBlockHeig
 
                     uniBet.resultType = finalOdds.second <= refundOdds ? BetResultType::betResultRefund : BetResultType::betResultWin;
                     // write payout height: result height + 1
-                    uniBet.payoutHeight = static_cast<uint32_t>(nNewBlockHeight);
+                    uniBet.payoutHeight = (uint32_t) nNewBlockHeight;
                 }
                 else {
                     uniBet.resultType = BetResultType::betResultLose;
