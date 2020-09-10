ce1502f8cdf616ec3539031eec86a26975c62206
random-zebra <random.zebra@protonmail.com>
2020-09-10 19:28:14 +0200

    Align GetBlockValue calls to CreateCoinStake: current height + 1

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -230,7 +230,7 @@ std::string BlockToString(CBlockIndex* pBlock)
     if (pBlock->nHeight == 0)
         Generated = OutVolume;
     else
-        Generated = GetBlockValue(pBlock->nHeight - 1);
+        Generated = GetBlockValue(pBlock->nHeight);
 
     std::string BlockContentCells[] =
         {
